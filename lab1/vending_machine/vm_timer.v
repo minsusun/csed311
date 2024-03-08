a9356100e2c8d6acc6bacb061eb0c6a7f4b14ce6
minsusun <poodding397@gmail.com>
2024-03-08 16:19:13 +0900

    lab1: moderate timer actions
    + force use `kNumWaitTime + 1 when coin inserted
    + force use 3 cycles before trigger_return using trigger_flag

--- lab1/vending_machine/vm_timer.v
+++ lab1/vending_machine/vm_timer.v
@@ -3,6 +3,7 @@
 module vm_timer(
     clk,
     reset_n,
+    i_trigger_return,
     i_input_coin,
     i_select_item,
     o_available_item,
@@ -10,23 +11,33 @@ module vm_timer(
 );
     input clk;
     input reset_n;
+    input i_trigger_return;
     input [`kNumCoins - 1: 0] i_input_coin;
     input [`kNumItems - 1: 0] i_select_item;
     input [`kNumItems - 1: 0] o_available_item;
     output reg [31: 0] wait_time;
 
+    reg trigger_flag;
+
     // initialize values
     initial begin
         wait_time = 0;
+        trigger_flag = 0;
     end
 
     always @(posedge clk) begin
         if(!reset_n)
             wait_time <= 0;
+        else if(!trigger_flag && i_trigger_return) begin
+            trigger_flag <= 1;
+            wait_time <= 3;
+        end
         else if(i_input_coin != 0 || (i_select_item & o_available_item) != 0)
-            wait_time <= `kWaitTime;
+            wait_time <= `kWaitTime + 1;
         else if(wait_time != 0)
             wait_time <= wait_time - 1;
+        else if(trigger_flag && wait_time == 0)
+            trigger_flag <= 0;
     end
 
 endmodule
