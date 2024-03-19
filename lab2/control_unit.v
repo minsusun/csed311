9a2e7ff5939e3229160dd35b3af92489165509f0
minsusun <poodding397@gmail.com>
2024-03-19 20:30:10 +0900

    lab2: control_unit impl

--- lab2/control_unit.v
+++ lab2/control_unit.v
@@ -1,15 +1,34 @@
+`include "opcodes.v"
+
 module control_unit(
-    input part_of_inst,
+    input [6:0] part_of_inst,
     output is_jal,
     output is_jalr,
     output branch,
     output mem_read,
     output mem_to_reg,
     output mem_write,
-    output alu_src,
+    output [1:0] alu_src_1,
+    output alu_src_2,
     output write_enable,
     output pc_to_reg,
     output is_ecall
 );
+
+assign is_jal = (part_of_inst == `JAL);
+assign is_jalr = (part_of_inst == `JALR);
+assign branch = (part_of_inst == `BRANCH);
+
+assign mem_read = (part_of_inst == `LOAD);
+assign mem_to_reg = (part_of_inst == `LOAD);
+assign mem_write = (part_of_inst == `STORE);
+
+assign alu_src_1 = 2'b00;
+assign alu_src_2 = (part_of_inst == `ARITHMETIC_IMM) || (part_of_inst == `JAL) || (part_of_inst == `JALR) || (part_of_inst == `STORE) || (part_of_inst == `LAOD);
+
+assign write_enable = (part_of_inst != `STORE) && (part_of_inst != `BRANCH)
+assign pc_to_reg = (part_of_inst == `JAL) || (part_of_inst == `JALR);
+
+assign is_ecall = (part_of_inst == `ECALL);
 
 endmodule
