762aa98e04066dd8afd100bc89fa2df5ca833678
minsusun <poodding397@gmail.com>
2024-04-16 20:14:10 +0900

    lab4-1: partial wiring

--- lab4-1/cpu.v
+++ lab4-1/cpu.v
@@ -13,13 +13,30 @@ module cpu(input reset,       // positive reset signal
            output is_halted, // Whehther to finish simulation
            output [31:0]print_reg[0:31]); // Whehther to finish simulation
   /***** Wire declarations *****/
+  /***** IF Stage *****/
+  wire [31:0] IF_inst;
+  /***** ID Stage *****/
+  wire ID_alu_op;
+  wire ID_alu_src;
+  wire ID_mem_write;
+  wire ID_mem_read;
+  wire ID_mem_to_reg;
+  wire ID_reg_write;
+  wire [31:0] ID_rs1_data;
+  wire [31:0] ID_rs2_data;
+  wire [31:0] ID_imm;
+  wire ID_ALU_ctrl_unit_input;
+  wire [ 5:0] ID_rd;
+  /***** EX Stage *****/
+  /***** MEM Stage *****/
+  /***** WB Stage *****/
   /***** Register declarations *****/
   // You need to modify the width of registers
   // In addition, 
   // 1. You might need other pipeline registers that are not described below
   // 2. You might not need registers described below
   /***** IF/ID pipeline registers *****/
-  reg IF_ID_inst;           // will be used in ID stage
+  reg [31:0] IF_ID_inst;           // will be used in ID stage
   /***** ID/EX pipeline registers *****/
   // From the control unit
   reg ID_EX_alu_op;         // will be used in EX stage
@@ -52,45 +69,48 @@ module cpu(input reset,       // positive reset signal
   reg MEM_WB_mem_to_reg;    // will be used in WB stage
   reg MEM_WB_reg_write;     // will be used in WB stage
   // From others
-  reg MEM_WB_mem_to_reg_src_1;
-  reg MEM_WB_mem_to_reg_src_2;
+  reg [4:0] MEM_WB_mem_to_reg_src_1;
+  reg [4:0] MEM_WB_mem_to_reg_src_2;
+  reg MEM_WB_is_halt;
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
   PC pc(
-    .reset(),       // input (Use reset to initialize PC. Initial value must be 0)
-    .clk(),         // input
+    .reset(reset),       // input (Use reset to initialize PC. Initial value must be 0)
+    .clk(clk),         // input
     .next_pc(),     // input
     .current_pc()   // output
   );
   
   // ---------- Instruction Memory ----------
   InstMemory imem(
-    .reset(),   // input
-    .clk(),     // input
+    .reset(reset),   // input
+    .clk(clk),     // input
     .addr(),    // input
-    .dout()     // output
+    .dout(IF_inst)     // output
   );
 
   // Update IF/ID pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      IF_ID_inst = 32'b0;
     end
     else begin
+      IF_ID_inst = IF_inst;
     end
   end
 
   // ---------- Register File ----------
   RegisterFile reg_file (
-    .reset (),        // input
-    .clk (),          // input
+    .reset (reset),        // input
+    .clk (clk),          // input
     .rs1 (),          // input
     .rs2 (),          // input
     .rd (),           // input
     .rd_din (),       // input
-    .write_enable (),    // input
-    .rs1_dout (),     // output
-    .rs2_dout (),      // output
+    .write_enable (MEM_WB_reg_write),    // input
+    .rs1_dout (ID_rs1_data),     // output
+    .rs2_dout (ID_rs2_data),      // output
     .print_reg(print_reg)
   );
 
@@ -98,27 +118,49 @@ module cpu(input reset,       // positive reset signal
   // ---------- Control Unit ----------
   ControlUnit ctrl_unit (
     .part_of_inst(),  // input
-    .mem_read(),      // output
-    .mem_to_reg(),    // output
-    .mem_write(),     // output
-    .alu_src(),       // output
-    .write_enable(),  // output
-    .pc_to_reg(),     // output
-    .alu_op(),        // output
+    .mem_read(ID_mem_read),      // output
+    .mem_to_reg(ID_mem_to_reg),    // output
+    .mem_write(ID_mem_write),     // output
+    .alu_src(ID_alu_src),       // output
+    .write_enable(ID_reg_write),  // output
+    .pc_to_reg(ID_mem_to_reg),     // output
+    .alu_op(ID_alu_op),        // output
     .is_ecall()       // output (ecall inst)
   );
 
   // ---------- Immediate Generator ----------
   ImmediateGenerator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(IF_ID_inst),  // input
+    .imm_gen_out(imm)    // output
   );
 
   // Update ID/EX pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      ID_EX_alu_op = 0;
+      ID_EX_alu_src = 0;
+      ID_EX_mem_write = 0;
+      ID_EX_mem_read = 0;
+      ID_EX_mem_to_reg = 0;
+      ID_EX_reg_write = 0;
+      ID_EX_rs1_data = 32'b0;
+      ID_EX_rs2_data = 32'b0;
+      ID_EX_imm = 32'b0;
+      ID_EX_ALU_ctrl_unit_input = 0;
+      ID_EX_rd = 5'b0;
     end
     else begin
+      ID_EX_alu_op = 0;
+      ID_EX_alu_src = ID_mem_write;
+      ID_EX_mem_write = ID_mem_write;
+      ID_EX_mem_read = ID_mem_read;
+      ID_EX_mem_to_reg = ID_mem_to_reg;
+      ID_EX_reg_write = ID_reg_write;
+      ID_EX_rs1_data = ID_rs1_data;
+      ID_EX_rs2_data = ID_rs2_data;
+      ID_EX_imm = ID_imm;
+      ID_EX_ALU_ctrl_unit_input = 0;
+      ID_EX_rd = ID_rd;
     end
   end
 
@@ -147,8 +189,8 @@ module cpu(input reset,       // positive reset signal
 
   // ---------- Data Memory ----------
   DataMemory dmem(
-    .reset (),      // input
-    .clk (),        // input
+    .reset (reset),      // input
+    .clk (clk),        // input
     .addr (),       // input
     .din (),        // input
     .mem_read (),   // input
@@ -159,6 +201,11 @@ module cpu(input reset,       // positive reset signal
   // Update MEM/WB pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      MEM_WB_mem_to_reg = 0;
+      MEM_WB_reg_write = 0;
+      MEM_WB_mem_to_reg_src_1 = 5'b0;
+      MEM_WB_mem_to_reg_src_2 = 5'b0;
+      MEM_WB_is_halt = 0;
     end
     else begin
     end
