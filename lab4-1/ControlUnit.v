47c15ad22bb36a09cc7108556039038f68d9e48c
Doyoung Kim <d0319@postech.ac.kr>
2024-04-26 18:21:51 +0900

    Fix: ControlUnit now takes is_hazard for input.

--- lab4-1/ControlUnit.v
+++ lab4-1/ControlUnit.v
@@ -2,12 +2,12 @@
 
 module ControlUnit(
     input [31:0] part_of_inst,
+    input is_hazard,
     output reg mem_read,
     output reg mem_to_reg,
     output reg mem_write,
     output reg alu_src,
     output reg write_enable,
-    output reg pc_to_reg,
     output reg [1:0] alu_op,
     output reg is_ecall
 );
@@ -16,10 +16,9 @@ module ControlUnit(
     always @(*) begin
         mem_read = (opcode == `LOAD);
         mem_to_reg = (opcode == `LOAD);
-        mem_write = (opcode == `STORE);
+        mem_write = (opcode == `STORE) && (!is_hazard);
         alu_src = (opcode != `ARITHMETIC) && (opcode != `BRANCH);
-        write_enable = (opcode != `STORE) && (opcode != `BRANCH);
-        pc_to_reg = (opcode == `JALR || opcode == `JAL);
+        write_enable = (opcode != `STORE) && (opcode != `BRANCH) && (!is_hazard);
         alu_op = {
             (opcode == `ARITHMETIC) || 
             (opcode == `ARITHMETIC_IMM) || 
