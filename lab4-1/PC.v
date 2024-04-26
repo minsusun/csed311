1df999172613f58a1e11a311c3ac2ad45bcd9b01
Doyoung Kim <d0319@postech.ac.kr>
2024-04-26 18:20:44 +0900

    Fix: make PC module only update when pc_write asserted.

--- lab4-1/PC.v
+++ lab4-1/PC.v
@@ -1,13 +1,14 @@
 module PC(
 	input reset,
 	input clk,
+	input pc_write,
 	input [31:0] next_pc,
 	output reg [31:0] current_pc
 );
 	always @(posedge clk) begin
 		if(reset) 
 			current_pc <= 0;
-		else
+		else if(pc_write)
 			current_pc <= next_pc;
 	end
 endmodule
