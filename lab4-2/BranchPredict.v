1bb2df0f06849a5147cab2ce6ed1ed1804eb7d80
Doyoung Kim <d0319@postech.ac.kr>
2024-05-12 23:14:28 +0900

    Fix: fixed assignments into non-blocking assignments

--- lab4-2/BranchPredict.v
+++ lab4-2/BranchPredict.v
@@ -6,11 +6,11 @@
 `define BP_SN 3     // Branch Prediction Strongly Not taken
 
 module BranchPredict(
-    input reset,
-    input clk,
-    input is_branch,        // Is branch prediction needed ?
-    input is_taken,         // Branch taken or not in real
-    output prediction       // 1: Predict the branch to be taken, 0: Predict the branch not to be taken
+  input reset,
+  input clk,
+  input is_branch,        // Is branch prediction needed ?
+  input is_taken,         // Branch taken or not in real
+  output prediction       // 1: Predict the branch to be taken, 0: Predict the branch not to be taken
 );
 
 reg [ 1: 0] state;
@@ -18,22 +18,22 @@ reg [ 1: 0] state;
 assign prediction = (state == `BP_ST || state == `BP_WT) ? 1 : 0;
 
 always @(posedge clk) begin
-    if (reset)
-        state = `BP_ST;
-    else if (is_branch) begin
-        case(state)
-            `BP_ST:
-                state = is_taken ? `BP_ST : `BP_WT;
-            `BP_WT:
-                state = is_taken ? `BP_ST : `BP_WN;
-            `BP_WN:
-                state = is_taken ? `BP_WT : `BP_SN;
-            `BP_SN:
-                state = is_taken ? `BP_WN : `BP_SN;
-            default:
-                state = 0;
-        endcase
-    end
+  if (reset)
+    state <= `BP_ST;
+  else if (is_branch) begin
+    case(state)
+      `BP_ST:
+        state <= is_taken ? `BP_ST : `BP_WT;
+      `BP_WT:
+        state <= is_taken ? `BP_ST : `BP_WN;
+      `BP_WN:
+        state <= is_taken ? `BP_WT : `BP_SN;
+      `BP_SN:
+        state <= is_taken ? `BP_WN : `BP_SN;
+      default:
+        state <= 0;
+    endcase
+  end
 end
 
 endmodule
